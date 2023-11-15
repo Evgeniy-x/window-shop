7c80ca4008a45f0d944c89ca65fd2b99ce8dd037
Evgeniy <yaremenko.x@gmail.com>
2023-11-15 23:57:15 +0200

    create BusinessCard

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from '../Layout';
+import BusinessCard from '../BusinessCard/BusinessCard';
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />} />
+      <Route index element={<BusinessCard />} />
     </Routes>
   );
 }
