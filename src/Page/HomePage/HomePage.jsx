57d17ef01b9db9255b0daaf222ba722cce6a053f
Evgeniy <yaremenko.x@gmail.com>
2023-11-17 23:48:27 +0200

    update HomePage

--- src/Page/HomePage/HomePage.jsx
+++ src/Page/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import css from './Home.module.css';
+import css from './HomePage.module.css';
 
 const Home = () => {
   return (
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from '../Layout';
-import Home from '../../Page/Home/Home';
+import HomePage from '../../Page/HomePage/HomePage';
 
 export function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+      </Route>
     </Routes>
   );
 }
