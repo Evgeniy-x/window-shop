aff7f5d6f7740a19becb67a56a1ac1ac194cdfbe
Evgeniy <yaremenko.x@gmail.com>
2023-11-20 16:53:18 +0200

    create Hero section

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,63 +1,48 @@
 import css from './Catalog.module.css';
 
-const Catalog = () => {
+export const Catalog = () => {
   return (
     <>
-      <section className={css.container}>
-        <div className={css.row}>
-          <h1>
-            <span>
-              Скління балконів "під ключ"
-              <br />
-            </span>
-            за 7 000 гривень!
-          </h1>
-          <div>
-            <div className={css.main_features}>
-              <div className={css.col}>
-                <div className={css.main_features_block}>
-                  {/* <img src="assets/img/main/icons/quality.png" alt=""> */}
-                  <p>
-                    Висока <br />
-                    якість
-                  </p>
-                </div>
-              </div>
+      <div className={css.section_header}>
+        <h2>Остекление балконов и лоджий</h2>
+        <div className={css.section_header_sub}></div>
+      </div>
 
-              <div className={css.col}>
-                <div className={css.main_features_block}>
-                  {/* <img src="assets/img/main/icons/time.png" alt=""> */}
-                  <p>
-                    Швидкий <br />
-                    монтаж
-                  </p>
-                </div>
-              </div>
-
-              <div className={css.col}>
-                <div className={css.main_features_block}>
-                  {/* <img className={small_img" src="assets/img/main/icons/guaranty.png" alt=""> */}
-                  <p>
-                    Гарантия <br />3 года
-                  </p>
-                </div>
-              </div>
-
-              <div className={css.col}>
-                <div className={css.main_features_block}>
-                  {/* <img className={small_img" src="assets/img/main/icons/delivery.png" alt=""> */}
-                  <p>
-                    Вывоз <br />
-                    мусора
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+      {/* <!-- Slider --> */}
+      <div className={css.glazing_slider}>
+        <div className={'glazing_block text_center wow fadeInUp'}>
+          {/* <img src="assets/img/glazing/icons/1.png" alt="#"> */}
+          <a className={css.tree_link}>Деревянное остекление</a>
         </div>
-      </section>
+        <div
+          className={'glazing_block text-center wow fadeInUp'}
+          data-wow-delay="0.1s"
+        >
+          {/* <img src="assets/img/glazing/icons/2.png" alt="#"> */}
+          <a className={css.aluminum_link}>Алюминиевое остекление</a>
+        </div>
+        <div
+          className={'glazing_block text-center wow fadeInUp'}
+          data-wow-delay="0.2s"
+        >
+          {/* <img src="assets/img/glazing/icons/3.png" alt="#"> */}
+          <a className="plastic_link">Остекление пластиковыми рамами</a>
+        </div>
+        <div
+          className={'glazing_block text-center wow fadeInUp'}
+          data-wow-delay="0.3s"
+        >
+          {/* <img src="assets/img/glazing/icons/4.png" alt="#"> */}
+          <a className="french_link">Французское остекление (панорамное)</a>
+        </div>
+        <div
+          className={'glazing_block text-center wow fadeInUp'}
+          data-wow-delay="0.4s"
+        >
+          {/* <img src="assets/img/glazing/icons/5.png" alt="#"> */}
+          <a className="rise_link">Остекление с выносом</a>
+        </div>
+      </div>
     </>
   );
 };
-
-export default Catalog;
