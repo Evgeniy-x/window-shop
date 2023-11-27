050836b19a6bea8cb05e8c4c054093fc2972720a
Evgeniy <yaremenko.x@gmail.com>
2023-11-27 10:59:37 +0200

    create OurWorks

---
+++ src/components/OurWorks/OurWorks.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+import css from './OurWorks.module.css';
+import woodenImg from '../../assets/images/glazing/icons/wooden.png';
+import aluminumImg from '../../assets/images/glazing/icons/aluminum.png';
+import plasticImg from '../../assets/images/glazing/icons/plastic.png';
+import frenchImg from '../../assets/images/glazing/icons/french.png';
+
+export const OurWorks = () => {
+  return <></>;
+};
