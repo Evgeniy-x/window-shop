184f3335c8d3f34d642a29040504c5fae80daf2e
Evgeniy <yaremenko.x@gmail.com>
2023-12-01 22:06:25 +0200

    add css for Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 export const Footer = () => {
   return (
     <>
-      <footer className="footer">
-        <div className="container">
+      <footer className={css.footer}>
+        <div className={css.container}>
           <div className={css.logo_link}>
             <Link to={`/`}>EUROBUD</Link>
           </div>
 
-          <div className="contacts">
+          <div className={css.contacts}>
             <a href="tel:+380999891100">+38 099-989-11-00</a>
             <a href="mailto:eurobud@gmail.com">evrobud@gmail.com</a>
           </div>
