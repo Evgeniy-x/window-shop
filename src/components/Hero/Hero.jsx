6f8ca669f81c24df416f2e065dfdb4538a3d9387
Evgeniy <yaremenko.x@gmail.com>
2023-12-03 11:06:21 +0200

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,8 @@
 import css from './Hero.module.css';
+import quality from '../../assets/images/hero/quality.png';
+import time from '../../assets/images/hero/time.png';
+import guaranty from '../../assets/images/hero/guaranty.png';
+import delivery from '../../assets/images/hero/delivery.png';
 
 export const Hero = () => {
   return (
@@ -13,28 +17,28 @@ export const Hero = () => {
             <div className={css.main_features}>
               <div className={css.col}>
                 <div className={css.main_features_block}>
-                  {/* <img src="assets/img/main/icons/quality.png" alt=""> */}
+                  <img src={quality} alt="quality" />
                   <p>Висока якість</p>
                 </div>
               </div>
 
               <div className={css.col}>
                 <div className={css.main_features_block}>
-                  {/* <img src="assets/img/main/icons/time.png" alt=""> */}
+                  <img src={time} alt="time" />
                   <p>Швидкий монтаж</p>
                 </div>
               </div>
 
               <div className={css.col}>
                 <div className={css.main_features_block}>
-                  {/* <img className={small_img" src="assets/img/main/icons/guaranty.png" alt=""> */}
+                  <img src={guaranty} alt="guaranty" />
                   <p>Гарантія 3 роки</p>
                 </div>
               </div>
 
               <div className={css.col}>
                 <div className={css.main_features_block}>
-                  {/* <img className={small_img" src="assets/img/main/icons/delivery.png" alt=""> */}
+                  <img src={delivery} alt="delivery" />
                   <p>Вивіз сміття</p>
                 </div>
               </div>
