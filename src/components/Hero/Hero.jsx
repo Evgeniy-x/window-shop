d3619d6e5374986d0d50219d6fce81dfaeda7dc5
Evgeniy <yaremenko.x@gmail.com>
2023-12-07 14:25:51 +0200

    editing styles in Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -13,32 +13,38 @@ export const Hero = () => {
           Скління балконів "під ключ" від 7 000 гривень!
         </h1>
         <div>
-          <div className={css.main_features}>
+          <div className={css.advantages}>
+            <div className={css.advantages_block}>
+              <img className={css.advantages_img} src={quality} alt="quality" />
+              <p className={css.advantages_text}>Висока якість</p>
+            </div>
+
             <div className={css.col}>
-              <div className={css.main_features_block}>
-                <img src={quality} alt="quality" />
-                <p>Висока якість</p>
+              <div className={css.advantages_block}>
+                <img className={css.advantages_img} src={time} alt="time" />
+                <p className={css.advantages_text}>Швидкий монтаж</p>
               </div>
             </div>
 
             <div className={css.col}>
-              <div className={css.main_features_block}>
-                <img src={time} alt="time" />
-                <p>Швидкий монтаж</p>
+              <div className={css.advantages_block}>
+                <img
+                  className={css.advantages_img}
+                  src={guaranty}
+                  alt="guaranty"
+                />
+                <p className={css.advantages_text}>Гарантія 3 роки</p>
               </div>
             </div>
 
             <div className={css.col}>
-              <div className={css.main_features_block}>
-                <img src={guaranty} alt="guaranty" />
-                <p>Гарантія 3 роки</p>
-              </div>
-            </div>
-
-            <div className={css.col}>
-              <div className={css.main_features_block}>
-                <img src={delivery} alt="delivery" />
-                <p>Вивіз сміття</p>
+              <div className={css.advantages_block}>
+                <img
+                  className={css.advantages_img}
+                  src={delivery}
+                  alt="delivery"
+                />
+                <p className={css.advantages_text}>Вивіз сміття</p>
               </div>
             </div>
           </div>
