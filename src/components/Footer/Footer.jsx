3c96652b8d20cf426c8907f0978c1bfbca3f2901
Evgeniy <yaremenko.x@gmail.com>
2023-12-10 23:33:14 +0200

    update Footer.css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,16 +4,14 @@ import { Link } from 'react-router-dom';
 export const Footer = () => {
   return (
     <>
-      <footer className={css.footer}>
-        <div className={css.container}>
-          <div className={css.logo_link}>
-            <Link to={`/`}>EUROBUD</Link>
-          </div>
+      <footer className={css.container}>
+        <Link to={`/`} className={css.logo_link}>
+          EUROBUD
+        </Link>
 
-          <div className={css.contacts}>
-            <a href="tel:+380999891100">+38 099-989-11-00</a>
-            <a href="mailto:eurobud@gmail.com">evrobud@gmail.com</a>
-          </div>
+        <div className={css.contacts}>
+          <a href="tel:+380999891100">+38 099-989-11-00</a>
+          <a href="mailto:eurobud@gmail.com">evrobud@gmail.com</a>
         </div>
       </footer>
     </>
