18d5a962ac77ebd0dbd57af3717b2dec1a303c6f
Evgeniy <yaremenko.x@gmail.com>
2024-01-01 23:39:31 +0200

    update French page

--- src/components/FrenchGlazing/FrenchGlazing.jsx
+++ src/components/FrenchGlazing/FrenchGlazing.jsx
@@ -1,3 +1,17 @@
+import css from './FrenchGlazing.module.css';
+
 export const FrenchGlazing = () => {
-  return <h2>French Glazing</h2>;
+  return (
+    <section className={css.container}>
+      <h2 className={css.french_title}>Панорамне скління </h2>
+      <p className={css.french_text}>Панорамне скління</p>
+      {/* <img src={french} className={css.french_img} alt="french" /> */}
+      <p className={css.french_text}>
+        Сучасне автоматизоване виробництво за запатентованими технологіями,
+        контроль якості на кожному етапі, адаптовані під кліматичні умови систем
+        монтажу, роблять нас лідерами за кількістю продажів ось уже більше 10
+        років.
+      </p>
+    </section>
+  );
 };
