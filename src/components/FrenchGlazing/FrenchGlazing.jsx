0194762116e8f1a9d49b6e23926ebebfe3e7b513
Evgeniy <yaremenko.x@gmail.com>
2024-01-04 23:52:48 +0200

    Update FrenchGlazing.jsx

--- src/components/FrenchGlazing/FrenchGlazing.jsx
+++ src/components/FrenchGlazing/FrenchGlazing.jsx
@@ -4,7 +4,12 @@ export const FrenchGlazing = () => {
   return (
     <section className={css.container}>
       <h2 className={css.french_title}>Панорамне скління </h2>
-      <p className={css.french_text}>Панорамне скління</p>
+      <p className={css.french_text}>
+        Панорамне скління - це вид скління будинків і квартир при якому великі
+        вікна встановлюються на всю зовнішню стіну від підлоги до стелі. Таке
+        скління забезпечує приголомшливий вид з вікна і максимальну кількість
+        природного світла в приміщенні
+      </p>
       {/* <img src={french} className={css.french_img} alt="french" /> */}
       <p className={css.french_text}>
         Сучасне автоматизоване виробництво за запатентованими технологіями,
